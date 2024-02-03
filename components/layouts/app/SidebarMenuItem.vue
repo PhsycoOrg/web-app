1f4727a28c54e2483c7671ac50bdd0c91496b3ee
Daniel López Sánchez <dlopezs2009@hotmail.com>
2024-02-03 18:45:45 -0500

    [FIX] hide sidebar on mobile when is cliked item

--- components/layouts/app/SidebarMenuItem.vue
+++ components/layouts/app/SidebarMenuItem.vue
@@ -1,6 +1,6 @@
 <template>
   <li v-if="subMenu == undefined" class="flex items-center w-full h-10 text-sm">
-    <NuxtLink activeClass="sidemenu-active" :to="to" class="text-black px-3 inline-flex items-center w-full h-full hover:bg-slate-100 rounded-xl">
+    <NuxtLink activeClass="sidemenu-active" :to="to" @click="handleMovileEvent" class="text-black px-3 inline-flex items-center w-full h-full hover:bg-slate-100 rounded-xl">
       <span v-html="icon"></span>
       <span class="font-medium">{{ text }}</span>
     </NuxtLink>
@@ -18,7 +18,7 @@
     
     <ul class="bg-white w-full py-2 pl-6 space-y-2 transform origin-top transition-all ease-in-out duration-300" :class="{'block': dropdownOpen, 'hidden': !dropdownOpen}">
       <li v-for="item in subMenu" class="px-4 py-2 text-sm hover:bg-slate-100 rounded-md">
-        <NuxtLink :to="item.to" active-class="sidemenu-submenu-active" class="inline-flex items-center w-full h-full">{{ item.text }}</NuxtLink>
+        <NuxtLink :to="item.to" active-class="sidemenu-submenu-active" @click="handleMovileEvent" class="inline-flex items-center w-full h-full">{{ item.text }}</NuxtLink>
       </li>
     </ul>
   </li>
@@ -45,9 +45,17 @@
   });
 
   const { text, to, icon, subMenu } = props;
+
+  const { $event } = useNuxtApp()
   const dropdownOpen = ref<boolean>(false);
+
   const toggleDropdown = (() => {
     dropdownOpen.value = !dropdownOpen.value;
+  });
+  const handleMovileEvent = (() => {
+    if (window.matchMedia("(max-width: 1023px)").matches) {
+      $event('layout:show-sidebar', false);
+    }
   });
   const handleOptionClick = ((option: any) => {
     console.log(`Clic en ${option}`);
--- components/user/profile/Information.vue
+++ components/user/profile/Information.vue
@@ -165,6 +165,4 @@
   });
 </script>
 
-<style>
-
-</style>
+<style></style>
