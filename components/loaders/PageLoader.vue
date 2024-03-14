a94bd0cc3485f38db4a2a9bfedb88b61fe244181
Daniel López Sánchez <dlopezs2009@hotmail.com>
2024-03-14 17:08:46 -0500

    create Page Loader component

---
+++ components/loaders/PageLoader.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="absolute bg-white w-full h-full z-10 flex justify-center items-center flex-col">
+    <l-squircle size="40" stroke="3" stroke-length="0.15" bg-opacity="0.1" speed="0.9" color="black"></l-squircle>
+    <p class="mt-4">{{ text }}</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { squircle } from 'ldrs'
+
+  const props = defineProps({
+    text: {
+      type: String,
+      required: true
+    },
+  });
+
+  const { text } = props;
+
+  squircle.register();
+</script>
+
+<style></style>
