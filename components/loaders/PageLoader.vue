93dbb00faa61532d007b5f64e110d1921773d2ff
Daniel López Sánchez <dlopezs2009@hotmail.com>
2024-03-14 17:38:37 -0500

    z-index on page loader

--- components/loaders/PageLoader.vue
+++ components/loaders/PageLoader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="absolute bg-white w-full h-full z-10 flex justify-center items-center flex-col">
+  <div class="absolute bg-white w-full h-full z-[2] flex justify-center items-center flex-col">
     <l-squircle size="40" stroke="3" stroke-length="0.15" bg-opacity="0.1" speed="0.9" color="black"></l-squircle>
     <p class="mt-4">{{ text }}</p>
   </div>
